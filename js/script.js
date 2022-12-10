cd91fcefe7676aa10266c035b4132fedb6a40e35
佐藤渚 <hakureireim8@gmail.com>
2022-12-11 06:03:17 +0900

    kusa

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const bgm1 = document.querySelector("#bgm1");       // <audio>
 const btn = document.querySelector("#btn-play");   // <button>
 
+console.log(location.hostname);
+
 btn.addEventListener("click", () => {
   // pausedがtrue=>停止, false=>再生中
   if (!bgm1.paused) {
@@ -13,6 +15,22 @@ btn.addEventListener("click", () => {
   }
 });
 
+function gate() {
+  // ▼ユーザの入力を求める
+  var UserInput = prompt("パスワードを入力して下さい:", "");
+  // ▼入力内容をチェック
+  if (/\W+/g.test(UserInput)) {
+    // ▼半角英数字以外の文字が存在したらエラー
+    alert("半角英数字のみを入力して下さい。");
+  }
+  // ▼キャンセルをチェック
+  else if (UserInput != null) {
+    // ▼入力内容からファイル名を生成して移動
+    location.href = UserInput + ".html";
+  }
+}
+
+
 function push_button() {
   document.location.href = "templates/nowtime/nowtime.html";
 }
@@ -23,4 +41,7 @@ function cookie_tests() {
 
 function music_play() {
   document.location.href = "templates/Musicplay/index.html"
+}
+function homeback() {
+  location.href = "/";
 }
--- templates/Musicplay/script.js
+++ templates/Musicplay/script.js
@@ -1,3 +1,37 @@
-function backpages() {
+const bgm1 = document.querySelector("#bgm1");       // <audio>
+const btn1 = document.querySelector("#btn-play1");   // <button>
+
+btn1.addEventListener("click", () => {
+    // pausedがtrue=>停止, false=>再生中
+    if (!bgm1.paused) {
+        btn1.innerHTML = '<span class="material-symbols-outlined">play_arrow</span>';  // 「再生ボタン」に切り替え
+        bgm1.pause();
+    }
+    else {
+        btn1.innerHTML = '<span class="material-symbols-outlined">pause</span>';  // 「一時停止ボタン」に切り替え
+        bgm1.play();
+    }
+});
+
+const bgm2 = document.querySelector("#bgm2");       // <audio>
+const btn2 = document.querySelector("#btn-play2");   // <button>
+
+btn2.addEventListener("click", () => {
+    // pausedがtrue=>停止, false=>再生中
+    if (!bgm2.paused) {
+        btn2.innerHTML = '<span class="material-symbols-outlined">play_arrow</span>';  // 「再生ボタン」に切り替え
+        bgm2.pause();
+    }
+    else {
+        btn2.innerHTML = '<span class="material-symbols-outlined">pause</span>';  // 「一時停止ボタン」に切り替え
+        bgm2.play();
+    }
+});
+
+function backpages1() {
     history.back();
+}
+
+function backpages2(){
+    location.href = "/"
 }
